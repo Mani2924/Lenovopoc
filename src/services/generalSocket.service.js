4e216fe7b7d175cc8f1dbb93c22968eabdc5c1e5
Teebanraj <teebanraj.dhanusu@twilightsoftwares.com>
2024-06-14 17:34:55 +0530

    12 hrs Formating live data to socket

--- src/services/generalSocket.service.js
+++ src/services/generalSocket.service.js
@@ -6,10 +6,10 @@ async function getFilteredData() {
   try {
     // Get current time in IST
     const nowIST = moment.tz('Asia/Kolkata');
-    
+
     // Calculate start and end hour for the current hour in IST
     const startHourIST = nowIST.clone().startOf('hour');
-    const endHourIST = startHourIST.clone().add(1, 'hour');   
+    const endHourIST = startHourIST.clone().add(1, 'hour');
     const startHourUTC = startHourIST.clone().utc().format();
     const endHourUTC = endHourIST.clone().utc().format();
 
@@ -17,22 +17,28 @@ async function getFilteredData() {
       where: {
         Op_Finish_Time: {
           [Op.gte]: startHourUTC,
-          [Op.lt]: endHourUTC
+          [Op.lt]: endHourUTC,
         },
       },
     });
 
     // Extract the hour component in 'HH' format (24-hour clock)
-    const startHour = startHourIST.format('HH');
-    const endHour = endHourIST.format('HH');
+    const startHour =
+      startHourIST.format('HH') < 12
+        ? startHourIST.format('HH')
+        : startHourIST.format('HH') - 12;
+    const endHour =
+      endHourIST.format('HH') < 12
+        ? endHourIST.format('HH')
+        : endHourIST.format('HH') - 12;
 
     return {
       totalCount,
       startHour,
-      endHour
+      endHour,
     };
   } catch (error) {
-    console.error("Error fetching total count data:", error);
+    console.error('Error fetching total count data:', error);
     throw error;
   }
 }
