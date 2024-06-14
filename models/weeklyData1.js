40f2c37662602154e37bb4e9732cc8f7e254e8b8
Teebanraj <teebanraj.dhanusu@twilightsoftwares.com>
2024-06-14 12:24:51 +0530

    WeeklyData1 model update

--- models/weeklyData1.js
+++ models/weeklyData1.js
@@ -1,17 +1,29 @@
-/* eslint-disable no-unused-vars */
 const { Model } = require('sequelize');
 const config = require('../src/config/vars');
+const { v4: uuidv4 } = require('uuid');
+
+// Function to generate custom IDs
+let nextId = 100;
+function generateCustomId() {
+  return nextId++;
+}
 
 module.exports = (sequelize, DataTypes) => {
   class weeklyData1 extends Model {}
   weeklyData1.init(
     {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        defaultValue: () => generateCustomId(), // Use custom ID generation function
+      },
       product_id: DataTypes.STRING,
       line: DataTypes.STRING,
       op_date: DataTypes.DATEONLY,
       start_time: DataTypes.TIME,
       end_time: DataTypes.TIME,
-
       target: DataTypes.INTEGER,
       totalcount: DataTypes.INTEGER,
       comments: DataTypes.STRING,
