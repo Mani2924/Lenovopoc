5a7e2150767a3256b861071bf7a936e4548966b5
Fernandus <fernandus.moris@broadcom.com>
2024-07-01 15:46:25 +0530

    every sec cron

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ const createSchema = async function () {
 createSchema();
 
 
-cron.schedule('1 * * * *', async () => {
+cron.schedule('* * * * * *', async () => {
   const currentTime = new Date().toLocaleTimeString();
   await generalService.hourlyData2();
 });
