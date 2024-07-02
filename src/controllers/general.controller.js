be467b0836099b226d0ba84250b20f07be50c24c
Fernandus <fernandus.moris@broadcom.com>
2024-07-02 10:46:35 +0530

    bug fixes

--- src/controllers/general.controller.js
+++ src/controllers/general.controller.js
@@ -1414,7 +1414,7 @@ userController.productionData = async (req, res, next) => {
     const overAllDetails = {
       overAllTarget: shiftADetails?.shiftTarget + shiftBDetails?.shiftTarget,
       overAllActual: shiftADetails?.shiftActual + shiftBDetails?.shiftActual,
-      overAllUPH:Math.round((shiftADetails?.shiftUPH + shiftBDetails?.shiftUPH) / ((shiftA.length + shiftB.length) +1 )) || 0,
+      overAllUPH:Math.round((shiftADetails?.shiftUPH + shiftBDetails?.shiftUPH) / 2) || 0,
       overAllOrdercount:
         shiftADetails?.mfgOrderCount + shiftBDetails?.mfgOrderCount,
       overAlldownTime:
