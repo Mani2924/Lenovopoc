0b7f858bb9b1f4531bdf439cd8facfb70cbe1406
Teebanraj <teebanraj.dhanusu@twilightsoftwares.com>
2024-07-04 20:12:25 +0530

    cron changes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const cron = require('node-cron');
-const moment = require('moment-timezone');
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const cron = require("node-cron");
+const moment = require("moment-timezone");
 
 // Custom Requires
-const { sequelize } = require('../models');
+const { sequelize } = require("../models");
 
-const config = require('./config/vars');
-const logger = require('./config/logger');
-const routes = require('./routes');
-const response = require('./utility/response');
-const rescodes = require('./utility/rescodes');
+const config = require("./config/vars");
+const logger = require("./config/logger");
+const routes = require("./routes");
+const response = require("./utility/response");
+const rescodes = require("./utility/rescodes");
 
-const generalService = require('./services/general.service');
+const generalService = require("./services/general.service");
 const {
   getFilteredData,
   getShiftData,
-} = require('./services/generalSocket.service');
-const xlsx = require('xlsx');
-const { sampleData } = require('../models/index');
+} = require("./services/generalSocket.service");
+const xlsx = require("xlsx");
+const { sampleData } = require("../models/index");
 
 // app express
 const app = express();
@@ -36,25 +36,25 @@ app.use(
 
 // cors Options
 const corsOptions = {
-  origin: '*',
+  origin: "*",
 };
 app.use(cors(corsOptions));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // API V1 Action Routes
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 // home handler
-app.use('/', (req, res, next) => {
+app.use("/", (req, res, next) => {
   res.response = { code: 404, message: rescodes.notFound };
   next();
 });
@@ -75,7 +75,7 @@ const createSchema = async function () {
 };
 createSchema();
 
-cron.schedule('*/59 * * * *', async () => {
+cron.schedule("1 * * * *", async () => {
   const currentTime = new Date().toLocaleTimeString();
   await generalService.hourlyData2();
 });
@@ -84,16 +84,16 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {});
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {});
 });
 
 // Cron job to emit data every 15 seconds
-cron.schedule('** * * * * *', async () => {
+cron.schedule("** * * * * *", async () => {
   try {
     // const target = getTarget();
     // console.log("target", target);
@@ -117,23 +117,23 @@ cron.schedule('** * * * * *', async () => {
 
     // console.log('result', result);
 
-    io.emit('dataUpdate', result);
+    io.emit("dataUpdate", result);
   } catch (error) {
-    console.error('Error while emitting data:', error);
+    console.error("Error while emitting data:", error);
   }
 });
 
 sequelize
   .sync({ logging: false })
   .then(() => {
-    logger.info('DB Connection Successful');
+    logger.info("DB Connection Successful");
 
     httpServer.listen(config.app.port, () => {
       logger.info(`Listening to port ${config.app.port}`);
     });
   })
   .catch((error) => {
-    logger.info('DB Connection Error');
+    logger.info("DB Connection Error");
     logger.info(error.message);
   });
 
